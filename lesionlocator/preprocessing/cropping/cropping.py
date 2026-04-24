ee2bae93f516ee3cba4e2d1b369328e6808955f7
Matthieu Asvazadourian <matthieu.asvazadourian@gmail.com>
2026-04-24 18:08:50 +0200

    Fix crop segmentation dtype

--- lesionlocator/preprocessing/cropping/cropping.py
+++ lesionlocator/preprocessing/cropping/cropping.py
@@ -3,6 +3,16 @@ from scipy.ndimage import binary_fill_holes
 from acvl_utils.cropping_and_padding.bounding_boxes import get_bbox_from_mask, bounding_box_to_slice
 
 
+def _dtype_can_represent_value(dtype, value):
+    dtype = np.dtype(dtype)
+    if np.issubdtype(dtype, np.integer):
+        info = np.iinfo(dtype)
+        return info.min <= value <= info.max
+    if np.issubdtype(dtype, np.floating):
+        return True
+    return False
+
+
 def create_nonzero_mask(data):
     """
 
@@ -32,6 +42,8 @@ def crop_to_nonzero(data, seg=None, nonzero_label=-1):
     data = data[slicer]
     if seg is not None:
         seg = seg[slicer]
+        if not _dtype_can_represent_value(seg.dtype, nonzero_label):
+            seg = seg.astype(np.promote_types(seg.dtype, np.min_scalar_type(nonzero_label)), copy=False)
         seg[(seg == 0) & (~nonzero_mask)] = nonzero_label
     else:
         seg = np.where(nonzero_mask, np.int8(0), np.int8(nonzero_label))
